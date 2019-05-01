553c4a04bf63354cd3f8fae2b224db43aa395dcc
quillasp <saralfddin93@yahoo.fr>
2019-05-01 13:50:50 +0200

    codecheck 2 done

--- partA2.cpp
+++ partA2.cpp
@@ -6,10 +6,10 @@ int main() {
   auto truc = new Int;
   auto truc1 = new Int(5);
   auto truc2 = new Int(*truc1);
-  auto truc3 = new Int(10);
+  auto truc3 = new Int(move(*truc2));
   
-  truc = truc1;
-  //truc1 = std::move(truc3);
+  *truc = *truc1;
+  *truc1 = move(*truc3);
   
   delete truc3;
   delete truc2;
